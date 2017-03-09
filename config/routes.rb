6bc6a870dab00c0a7265f964800edaa6fa2385ec
Kelvin <kelvinmei1996@gmail.com>
2017-03-09 11:29:46 -0500

    Added rails ch8

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get 'events/new'
   root 'events#index'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
 
 end
