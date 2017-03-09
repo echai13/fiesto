6ca76395193e99ef2b9515c09c006da7a3d042f6
Kelvin <kelvinmei1996@gmail.com>
2017-03-09 16:08:27 -0500

    Restricted to Brandeis.edu

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   before_save { self.email = email.downcase }
   validates :username, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[brandeis]+\.[edu]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
