ea201aae744228d834dc9cc8221d202bd037a07e
Kelvin <kelvinmei1996@gmail.com>
2017-03-15 14:30:01 -0400

    small changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   attr_accessor :remember_token
 
   before_save { self.email = email.downcase }
-  validates :username, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { maximum: 50 },
+                       uniqueness: {case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[brandeis]+\.[edu]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
