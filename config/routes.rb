8c860820e707990e4fb9c124dd64cb4a3e6e5329
echai13 <ericachai@Ericas-MacBook-Air.local>
2017-03-15 15:01:31 -0400

    Change header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   resources :events, :users
+  root 'static_pages#home'
+
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+
   get '/signup', to: 'users#new'
   get 'events/new'
-  root 'events#index'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-
 
 end
