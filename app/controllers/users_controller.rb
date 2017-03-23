29a963db6e87a24029d36a44a31461269bad9c11
Kelvin <kelvinmei1996@gmail.com>
2017-03-23 16:00:49 -0400

    pushing heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,12 @@ class UsersController < ApplicationController
      @user = User.find(params[:id])
   end
 
+  def settings
+    @user = User.find_by id: session[:user_id]
+    render 'setting'
+
+  end
+
   def create
    @user = User.new(user_params)
    if @user.save
@@ -23,6 +29,9 @@ class UsersController < ApplicationController
    def user_params
      params.require(:user).permit(:username, :email, :password,
                                   :password_confirmation)
+
+
    end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,16 @@ Rails.application.routes.draw do
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-
   get '/signup', to: 'users#new'
+  get '/signup', to: 'static_pages#home'
+  get '/settings', to: 'users#settings'
   get 'events/new'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+
+
 
 end
