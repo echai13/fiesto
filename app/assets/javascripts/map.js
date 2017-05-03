ad18334313735e76d15b616f33ff2c0d1cc3fe4e
Kelvin <kelvinmei1996@gmail.com>
2017-05-03 15:48:36 -0400

    updated js location

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,11 @@
+function initMap() {
+    var display = gon.latlong
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: display
+    });
+    var marker = new google.maps.Marker({
+        position: display,
+        map: map
+    });
+}
